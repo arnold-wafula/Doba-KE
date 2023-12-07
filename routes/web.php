d596f423044e841aeb2db1cd193611620b1c6b1e
Arnold Wafula <arnold.wafula0@gmail.com>
2023-12-07 09:10:18 +0300

    Added new routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/music', function () {
+    return view('');
+});
+
+Route::get('/videos', function() {
+    return view('videos');
+});
